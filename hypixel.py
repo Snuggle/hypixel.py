309b22629dedd095ec8dabdc19ce3053428eacce
Snuggle <snuggle@sprinkly.net>
2018-02-12 22:12:57 +0000

    Bumping version

--- hypixel.py
+++ hypixel.py
@@ -1,5 +1,5 @@
 """ Simple Hypixel-API in Python, by Snuggle | 2017-09-30 to 2017-10-28 """
-__version__ = '0.7.5'
+__version__ = '0.7.6'
 # pylint: disable=C0103
 # TODO: Add more comments. Explain what's happening!
 # TODO: Add API-usage stat-tracking. Like a counter of the number of requests and how many per minute etc.
